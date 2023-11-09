6384b86288b2818a8ec3b41fdd25e65468e455a6
SudeepGowda55 <ssudeepgowda55@gmail.com>
2023-11-09 21:48:06 +0530

    search option added

--- app/page.tsx
+++ app/page.tsx
@@ -6,29 +6,40 @@ import { Relays, Pool } from "@/utils/nostr";
 
 export default function Home() {
 
-  const [message, setMessage] = useState<Event | null>(null);
+  const [messages, setMessages] = useState<Event[]>([]);
 
   useEffect(() => {
     if (!Pool) return;
- 
+
     const sub = Pool.sub(Relays, [
       {
         kinds: [1],
-        limit: 10,
-        "#t": ["nostr"]
+        limit: 20,
       }
     ])
 
     sub.on("event", (event: Event) => {
-      console.log(event);
+      setMessages((prev) => [...prev, event]);
     })
 
   }, [Pool])
 
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 max-w-[100vw]">
       <p> Nostr Client </p>
       <div>
+        {(messages.length) > 0 ?
+          (messages.map((message) => (
+            <div key={message.id} className="flex flex-col">
+              <p className='bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 text-black my-6 mx-[8vw] p-10 rounded-2xl'> {message.content} </p>
+            </div>
+          )))
+          :
+          <p className='text-center m-7'>
+            Content Loading
+          </p>
+        }
       </div>
     </main>
   )
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -7,6 +7,7 @@ import { Event } from 'nostr-tools';
 const Search = () => {
 
     const [hashtag, setHashtag] = useState<string | null>(null);
+    const [searches, setSearches] = useState<Event[]>([]);
 
     const hashTag = (e: React.ChangeEvent<HTMLInputElement>) => {
         setHashtag(e.target.value)
@@ -23,29 +24,42 @@ const Search = () => {
                 }
             ])
             sub.on("event", (event: Event) => {
-                console.log(event);
+                setSearches((prev) => [...prev, event]);
             })
         }
     }
 
     return (
         <>
-            <div>
+            <div className='m-4'>
                 <h1 className="text-3xl font-bold">Search for Posts</h1>
             </div>
             <div>
-                <form onSubmit={(e: React.FormEvent<HTMLFormElement>) => search(e)}>
-                    <label className="block">Enter the hashtag</label>
+                <form onSubmit={(e: React.FormEvent<HTMLFormElement>) => search(e)} className='flex flex-row items-center justify-between mx-[10vw] w-[30vw]'>
+                    <label className="">Enter the hashtag</label>
                     <input
                         type="text"
                         placeholder="nostr"
                         name="key"
                         required
-                        className="text-black block my-2"
+                        className="text-black inline-block my-2 rounded-lg" 
                         onChange={(e: React.ChangeEvent<HTMLInputElement>) => hashTag(e)}
                     />
                     <button type="submit">Search</button>
                 </form>
+            </div>
+            <div>
+                {(searches.length) > 0 ?
+                    (searches.map((event) => (
+                        <div key={event.id} className="flex flex-col">
+                            <p className='bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 text-black my-6 mx-[8vw] p-10 rounded-2xl'> {event.content} </p>
+                        </div>
+                    )))
+                    :
+                    <p className='text-center m-7'>
+                        Please Search
+                    </p>
+                }
             </div>
         </>
     )
